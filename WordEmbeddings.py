e4f7f8e9b76054e4039740008ac619961b708bf1
srbhr <srbh077@gmail.com>
2020-03-18 01:25:33 +0530

    Modified the WordEmbeddings File

--- WordEmbeddings.py
+++ WordEmbeddings.py
@@ -1,39 +1,49 @@
 class WordEmbeddings:
-    
-    try:
-        import spacy
-        engnlp = spacy.load('en_core_web_md')
-        spanlp = spacy.load('es_core_news_md')
-    except ImportError:
-            print("Module Not Available")
-    
-    def __init__(self, text = "abcdefghijklmnopqrstuvwxyz"):
+
+    import spacy
+    spanlp = spacy.load('es_core_news_md')
+    engnlp = spacy.load('en_core_web_md')
+
+    def __init__(self, text="abcdefghijklmnopqrstuvwxyz"):
         self.text = text
-        
-    def get_spanish_vectors(self, remove_nulls=True):
-        doc = spanlp(self.text)
+
+    def get_spanish_vectors(self, remove_nulls=True, normalized=True):
+
+        doc = self.spanlp(self.text)
         vector = []
         if remove_nulls == True:
             for token in doc:
                 if token.has_vector == True:
-                    vector.append([token.text, token.vector_norm])
+                    if normalized == True:
+                        vector.append([token.text, token.vector_norm])
+                    else:
+                        vector.append([token.text, token.vector])
+
         else:
             for token in doc:
                 vector.append([token.text, token.vector_norm])
-                
+
         return vector
-    
-    def get_english_vectors(self, remove_nulls=True):
-        doc = engnlp(self.text)
+
+    def get_english_vectors(self, remove_nulls=True, normalized=True):
+
+        doc = self.engnlp(self.text)
         vector = []
         if remove_nulls == True:
             for token in doc.tokens:
                 if token.has_vector == True:
-                    vector.append([token.text, token.vector_norm])
+                    if normalized == True:
+                        vector.append([token.text, token.vector_norm])
+                    else:
+                        vector.append([token.text, token.vector])
         else:
             for token in doc.tokens:
                 vector.append([token.text, token.vector_norm])
-                
+
         return vector
-        
-        
+
+# ++++++++++++++++MAIN+++++++++++++++++++
+
+
+# word = WordEmbeddings(text='Gracias').get_spanish_vectors()
+# print(word)
